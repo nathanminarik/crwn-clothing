fbe32630e1221e0e1a5ca50a8e19a67ef1127148
Nathan Minarik <nminarik@medallia.com>
2020-02-03 19:14:38 -0500

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.scss';
+import { Route, Switch } from 'react-router-dom';
 
 import { HomePage } from './pages/homepage/homepage.component';
 
+const HatsPage = (props) => {
+	console.log(props)
+	return (
+		<div>
+			<h1>Hats</h1>
+		</div>
+	)
+};
+
 function App() {
-  return <HomePage />
+	return (
+		<Switch>
+			<Route path='/' exact component={HomePage} />
+			<Route path='/hats' component={HatsPage} />
+		</Switch>
+	)
 }
 
 export default App;
